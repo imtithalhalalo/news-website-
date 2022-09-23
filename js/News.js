1022ba98910f104e6661531af52011db31ca36da
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-23 14:29:58 +0300

    add news canvas

---
+++ js/News.js
@@ -0,0 +1,14 @@
+export const News = (element) => {
+    return (`
+    <div class="col-12 col-lg-4 py-4" >
+        <div class="box" id="${element.id}">
+            <img class="img-fluid w-100" src="${element.image}" id="image" alt="" />
+            <h1 class="container" id="title">${element.title}</h1>
+            <p class="container" id="text">
+            ${element.text}
+            </p>
+        </div>  
+    </div>
+        
+    `)
+}
