4091e5f3c697c4daf00bae48bd7c2fce91ab2283
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-23 20:19:36 +0300

    update canvas to have url from db

--- js/News.js
+++ js/News.js
@@ -1,14 +1,19 @@
 export const News = (element) => {
     return (`
     <div class="col-12 col-lg-4 py-4" >
-        <div class="box" id="${element.id}">
-            <img class="img-fluid w-100" src="${element.image}" id="image" alt="" />
-            <h1 class="container" id="title">${element.title}</h1>
-            <p class="container" id="text">
-            ${element.text}
-            </p>
-        </div>  
+        <a
+        href="${element.url_website}"
+        >
+            <div class="box" id="${element.id}">
+                
+                <img class="img-fluid w-100" src="${element.image}" id="image" alt="" />
+                <h1 class="container" id="title">${element.title}</h1>
+                <p class="container" id="text">
+                ${element.text}
+                </p>
+            </div>
+        </a> 
     </div>
-        
+     
     `)
 }
