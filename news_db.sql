e2b5deb8b74fd38761f92fbb315390defc08f3f7
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-23 20:18:45 +0300

    udate db to add url

--- news_db.sql
+++ news_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2022 at 01:25 PM
+-- Generation Time: Sep 23, 2022 at 06:57 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -31,18 +31,19 @@ CREATE TABLE `news` (
   `id` int(11) NOT NULL,
   `image` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `text` varchar(255) NOT NULL
+  `text` varchar(255) NOT NULL,
+  `url_website` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`id`, `image`, `title`, `text`) VALUES
-(1, 'https://images.wsj.net/im-628834/?width=860&size=1.5', 'World’s Central Banks Raise Rates After Fed Increase', 'The Bank of England raised its key interest rate for the seventh consecutive time in a flurry of central-bank meetings from Norway to South Africa that analysts billed as “Super Thursday.”'),
-(2, 'https://images.wsj.net/im-628906?width=860&size=1.3897937024972855', 'World’s Central Banks Race to Raise Rates After Fed Increase', 'LONDON—Central banks around the world moved Thursday to combat the effects of a soaring dollar and rising inflation, joining the Federal Reserve in risking a recession to rein in climbing prices.\r\n\r\n'),
-(3, 'https://images.wsj.net/im-628897?width=700&size=1.5005861664712778', 'Japan to Reopen to Tourists After 2 1/2 Years of Mostly Closed Borders', 'Japan said it would reopen the country next month to regular tourism, hoping to leverage the cheap yen to attract visitors and lift its sluggish economy.'),
-(4, 'https://images.wsj.net/im-628897?width=700&size=1.5005861664712778', 'Japan to Reopen to Tourists After 2 1/2 Years of Mostly Closed Borders', 'Japan said it would reopen the country next month to regular tourism, hoping to leverage the cheap yen to attract visitors and lift its sluggish economy.');
+INSERT INTO `news` (`id`, `image`, `title`, `text`, `url_website`) VALUES
+(1, 'https://images.wsj.net/im-628834/?width=860&size=1.5', 'World’s Central Banks Raise Rates After Fed Increase', 'The Bank of England raised its key interest rate for the seventh consecutive time in a flurry of central-bank meetings from Norway to South Africa that analysts billed as “Super Thursday.”', 'https://www.wsj.com/articles/war-inflation-knock-world-economy-off-balance-11663927566'),
+(2, 'https://images.wsj.net/im-628906?width=860&size=1.3897937024972855', 'World’s Central Banks Race to Raise Rates After Fed Increase', 'LONDON—Central banks around the world moved Thursday to combat the effects of a soaring dollar and rising inflation, joining the Federal Reserve in risking a recession to rein in climbing prices.\r\n\r\n', 'https://www.wsj.com/articles/war-inflation-knock-world-economy-off-balance-11663927566'),
+(3, 'https://images.wsj.net/im-628897?width=700&size=1.5005861664712778', 'Japan to Reopen to Tourists After 2 1/2 Years of Mostly Closed Borders', 'Japan said it would reopen the country next month to regular tourism, hoping to leverage the cheap yen to attract visitors and lift its sluggish economy.', 'https://www.wsj.com/articles/war-inflation-knock-world-economy-off-balance-11663927566'),
+(4, 'https://images.wsj.net/im-628897?width=700&size=1.5005861664712778', 'Japan to Reopen to Tourists After 2 1/2 Years of Mostly Closed Borders', 'Japan said it would reopen the country next month to regular tourism, hoping to leverage the cheap yen to attract visitors and lift its sluggish economy.', 'https://www.wsj.com/articles/war-inflation-knock-world-economy-off-balance-11663927566');
 
 --
 -- Indexes for dumped tables
